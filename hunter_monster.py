ab4bed7beec93e9eefb6a71c6b5315ec9cdea735
Sohyul1 <aerondequito967@gmail.com>
2025-02-24 17:19:17 +0800

    try 1

---
+++ hunter_monster.py
@@ -0,0 +1,11 @@
+# Import random 
+# Make the user's stats
+# Make the list of monsters and their stats
+# Ask thae player to start the game
+# Ask the player to choose between "train" or "fight"
+# Train - randomly increase the player's atck, def, and hp
+# Fight -  will fight a random monster from the list created 
+# Start of battle - player atcks first then monster(repeated until one of them dies )
+# Player won - rank them base on the monster they defeated and display a congratulatory message
+# Monster won - display a death message
+# Ask for if they want to star a new life 
