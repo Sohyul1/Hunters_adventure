7abe4631bbc9274b1b0c6fec6c098f17862ed653
Sohyul1 <aerondequito967@gmail.com>
2025-02-24 18:01:59 +0800

    making list of list and choosingf random enemy

--- hunter_monster.py
+++ hunter_monster.py
@@ -1,15 +1,23 @@
 # Import random 
 import random
+
 # Make the user's stats
 user_atck = 1
 user_def = 3
 user_hp = 10
+
 # Make the list of monsters and their stats
-monsters_and_stats = ["Slime", "Goblin", "Orc","Minotaur", "Dragon", ]
+monsters_and_stats = [["Slime", 1, 5, 8], 
+                      ["Goblin", 5, 8, 15], 
+                      ["Orc", 10, 15, 20],
+                      ["Minotaur", 15, 20, 25], 
+                      ["Dragon", 20, 25, 50] ]
+
 # Ask thae player to start the game
 ans = input("Type enter to start the game: ").lower().strip()
 # Ask the player to choose between "train" or "fight"
 if ans == "enter":
+    print('Note:"Train" will give you random addional stats.\n     "Fight" is battling a random monster.')
     choice = input("What would you like to do, 'Train' or 'Fight': ").lower().strip()
     
 # Train - randomly increase the player's atck, def, and hp
@@ -25,7 +33,13 @@ if ans == "enter":
         print(f"Your current stats are:\nAttack:{user_atck}\nDefense:{user_def}\nHP:{user_hp}")
 # Fight -  will fight a random monster from the list created 
     elif choice == "fight":
-        print("haha")
+        enemy = random.choice(monsters_and_stats)
+        enemy_name = enemy[0]
+        enemy_atck = enemy[1]
+        enemy_def = enemy [2]
+        enemy+hp = enemy[3]
+
+        
 
 
 # Start of battle - player atcks first then monster(repeated until one of them dies )
