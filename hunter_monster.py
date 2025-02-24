f29fa1dd62e6702ed299e17ace95e47691b4292c
Sohyul1 <aerondequito967@gmail.com>
2025-02-24 17:30:07 +0800

    2

--- hunter_monster.py
+++ hunter_monster.py
@@ -1,7 +1,13 @@
 # Import random 
+import random
 # Make the user's stats
+user_atck = 1
+user_def = 3
+user_hp = 10
 # Make the list of monsters and their stats
+monsters_and_stats = ["Slime", "Goblin", "Orc","Minotaur", "Dragon", ]
 # Ask thae player to start the game
+input("Type enter to start the game: ")
 # Ask the player to choose between "train" or "fight"
 # Train - randomly increase the player's atck, def, and hp
 # Fight -  will fight a random monster from the list created 
