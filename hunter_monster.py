ec3d7fd9230656f5f290264a30519a7d0809d9dc
Sohyul1 <aerondequito967@gmail.com>
2025-02-24 19:23:25 +0800

    change some wordings in the output

--- hunter_monster.py
+++ hunter_monster.py
@@ -2,88 +2,99 @@
 import random
 
 # Make the user's stats
-user_atck = 1
-user_def = 3
-user_hp = 10
+while True:
+    user_atck = 1
+    user_def = 3
+    user_hp = 10
 
-# Make the list of monsters and their stats
-monsters_and_stats = [["Slime", 1, 5, 8], 
-                      ["Goblin", 5, 8, 15], 
-                      ["Orc", 10, 15, 20],
-                      ["Minotaur", 15, 20, 25], 
-                      ["Dragon", 20, 25, 50] ]
+    # Make the list of monsters and their stats
+    monsters_and_stats = [["Slime", 1, 5, 8], 
+                        ["Goblin", 5, 8, 15], 
+                        ["Orc", 10, 15, 20],
+                        ["Minotaur", 15, 20, 25], 
+                        ["Dragon", 20, 25, 50] ]
 
-# Ask thae player to start the game
-ans = input("Type enter to start the game: ").lower().strip()
-# Ask the player to choose between "train" or "fight"
-if ans == "enter":
-    print('Note:\n"Train" will give you random addional stats.\n"Fight" is battling a random monster.')
+    # Ask thae player to start the game
+    ans = input("Type enter to start the game: ").lower().strip()
     
-    while True:
-        choice = input("\nWhat would you like to do, 'Train' or 'Fight': ").lower().strip()
+    # Ask the player to choose between "train" or "fight"
+    if ans == "enter":
+        print('Note:\n"Train" will give you random addional stats.\n"Fight" is battling a random monster.')
         
-# Train - randomly increase the player's atck, def, and hp
-        if choice == "train":
-            atck_incr = random.randint(1, 10)
-            def_incr = random.randint(1, 10)
-            hp_incr = random.randint(1, 10)
+        while True:
+            choice = input("\nType 'Train' to gain stats\nType 'Fight' to battle an enemy: ").lower().strip()
+            
+    # Train - randomly increase the player's atck, def, and hp
+            if choice == "train":
+                atck_incr = random.randint(1, 10)
+                def_incr = random.randint(1, 10)
+                hp_incr = random.randint(1, 10)
 
-            user_atck += atck_incr
-            user_def += def_incr
-            user_hp += hp_incr
-            print(f"You have gained {atck_incr} Attack, {def_incr} Defense, and {hp_incr} HP")
-            print(f"Your current stats are:\nAttack:{user_atck}\nDefense:{user_def}\nHP:{user_hp}")
-# Fight -  will fight a random monster from the list created 
-        elif choice == "fight":
-            break
-        else:
-            print("Invalid input, please input a 'Train' or 'Fight'. ")
+                user_atck += atck_incr
+                user_def += def_incr
+                user_hp += hp_incr
+                print("\nYou have increased your stats!")
+                print(f"Attack increased: +{atck_incr}\nDefense increased: +{def_incr}\nHP increased: +{hp_incr}\n")
+                print(f"Your current stats are:\nAttack:{user_atck}\nDefense:{user_def}\nHP:{user_hp}")
     
-    enemy = random.choice(monsters_and_stats)
-    enemy_name = enemy[0]
-    enemy_atck = enemy[1]
-    enemy_def = enemy [2]
-    enemy_hp = enemy[3]
+    # Fight -  will fight a random monster from the list created 
+            elif choice == "fight":
+                break
+            else:
+                print("Invalid input, please input a 'Train' or 'Fight'. ")
+        
+        enemy = random.choice(monsters_and_stats)
+        enemy_name = enemy[0]
+        enemy_atck = enemy[1]
+        enemy_def = enemy [2]
+        enemy_hp = enemy[3]
 
-    print(f"A wild {enemy_name} appeared!!!")
-    print("Get ready to battle!\n")
+        print(f"A wild {enemy_name} appeared!!!")
+        print(f"\nYour current stats are:\nAttack:{user_atck}\nDefense:{user_def}\nHP:{user_hp}")
+        print(f"\nThe {enemy_name} stats are:\nAttack:{enemy_atck}\nDefense{enemy_def}\nHP:{enemy_hp}\n")
+        print("Get ready to battle!\n")
 
 
-# Start of battle - player atcks first then monster (repeated until one of them dies )
-    while user_hp > 0 and enemy_hp > 0:
-        if user_atck > enemy_def:
-                user_dmg = user_atck - enemy_def
-        else:
-            user_dmg = 1
+    # Start of battle - player atcks first then monster (repeated until one of them dies )
+        while user_hp > 0 and enemy_hp > 0:
+            if user_atck > enemy_def:
+                    user_dmg = user_atck - enemy_def
+            else:
+                user_dmg = 1
 
-        enemy_hp -= user_dmg
-        print(f"You dealt {user_def} to the {enemy_name}!\n")
-        print(f"The {enemy_name} is now {enemy_hp} HP!\n")
-
-        if enemy_hp <= 0:
-            print(f"The {enemy_name} have died, You won!\n")
-# Player won - rank them base on the monster they defeated and display a congratulatory message
-            if enemy_name == "Slime":
-                 print("Congratulations! You are now a D-rank hunter!")
-            elif enemy_name == "Goblin":
-                 print("Congratulations! You are now a C-rank hunter!")
-            elif enemy_name == "Orc":
-                 print("Congratulations! You are now a B-rank hunter!")
-            elif enemy_name == "Minotaur":
-                 print("Congratulations! You are now an A-rank hunter!")
-            elif enemy_name == "Dragon":
-                 print("Congratulations! You are now an S-rank hunter!")
-                 break
-        if enemy_atck > user_def:
+            enemy_hp -= user_dmg
+            print(f"You dealt {user_def} to the {enemy_name}! Monster HP: {enemy_hp}\n")
+        
+            if enemy_hp <= 0:
+                print(f"The {enemy_name} have died, You won!\n")
+    
+    # Player won - rank them base on the monster they defeated and display a congratulatory message
+                if enemy_name == "Slime":
+                    print("Congratulations! You are now a D-rank hunter!")
+                elif enemy_name == "Goblin":
+                    print("Congratulations! You are now a C-rank hunter!")
+                elif enemy_name == "Orc":
+                    print("Congratulations! You are now a B-rank hunter!")
+                elif enemy_name == "Minotaur":
+                    print("Congratulations! You are now an A-rank hunter!")
+                elif enemy_name == "Dragon":
+                    print("Congratulations! You are now an S-rank hunter!")
+                break
+            
+            if enemy_atck > user_def:
                 enemy_dmg = enemy_atck - user_def
-        else:
+            else:
                 enemy_dmg = 1
 
-        user_hp -= enemy_dmg
-        print(f"The {enemy_name} dealt {enemy_dmg} damage to you!\n")
-        print (f"You are now {user_hp} HP!\n")
- # Monster won - display a death message
-        if user_hp <= 0:
-                print(f"You have died")
+            user_hp -= enemy_dmg
+            print(f"The {enemy_name} dealt {enemy_dmg} damage to you! Your HP:{user_hp}\n")
+            
+    # Monster won - display a death message
+            if user_hp <= 0:
+                    print(f"You have died in a embarassing way")
 
-# Ask for if they want to star a new life
+    # Ask for if they want to star a new life
+        ans = input("The game stops here, wanna play again? (yes/no) ").lower().strip()
+        if ans == "no":
+            print("Thank you for playing the game!")
+            break
