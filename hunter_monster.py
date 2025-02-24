ec9572996c1bfd81e05a0cbaa437e05dd88e762e
Sohyul1 <aerondequito967@gmail.com>
2025-02-24 19:37:08 +0800

    made a loop for the whole code and change some character stats

--- hunter_monster.py
+++ hunter_monster.py
@@ -3,23 +3,23 @@ import random
 
 # Make the user's stats
 while True:
-    user_atck = 1
+    user_atck = 5
     user_def = 3
     user_hp = 10
 
     # Make the list of monsters and their stats
-    monsters_and_stats = [["Slime", 1, 5, 8], 
-                        ["Goblin", 5, 8, 15], 
-                        ["Orc", 10, 15, 20],
-                        ["Minotaur", 15, 20, 25], 
-                        ["Dragon", 20, 25, 50] ]
+    monsters_and_stats = [["Slime", 5, 5, 8], 
+                        ["Goblin", 10, 8, 15], 
+                        ["Orc", 16, 15, 25],
+                        ["Minotaur", 25, 25, 50], 
+                        ["Dragon", 45, 40, 100] ]
 
     # Ask thae player to start the game
     ans = input("Type enter to start the game: ").lower().strip()
     
     # Ask the player to choose between "train" or "fight"
     if ans == "enter":
-        print('Note:\n"Train" will give you random addional stats.\n"Fight" is battling a random monster.')
+        print('\nNote:\n"Train" will give you random addional stats.\n"Fight" is battling a random monster.')
         
         while True:
             choice = input("\nType 'Train' to gain stats\nType 'Fight' to battle an enemy: ").lower().strip()
@@ -49,9 +49,9 @@ while True:
         enemy_def = enemy [2]
         enemy_hp = enemy[3]
 
-        print(f"A wild {enemy_name} appeared!!!")
+        print(f"\nA wild {enemy_name} appeared!!!")
         print(f"\nYour current stats are:\nAttack:{user_atck}\nDefense:{user_def}\nHP:{user_hp}")
-        print(f"\nThe {enemy_name} stats are:\nAttack:{enemy_atck}\nDefense{enemy_def}\nHP:{enemy_hp}\n")
+        print(f"\nThe {enemy_name} stats are:\nAttack: {enemy_atck}\nDefense {enemy_def}\nHP: {enemy_hp}\n")
         print("Get ready to battle!\n")
 
 
@@ -63,7 +63,7 @@ while True:
                 user_dmg = 1
 
             enemy_hp -= user_dmg
-            print(f"You dealt {user_def} to the {enemy_name}! Monster HP: {enemy_hp}\n")
+            print(f"You dealt {user_dmg} to the {enemy_name}! Monster HP: {enemy_hp}\n")
         
             if enemy_hp <= 0:
                 print(f"The {enemy_name} have died, You won!\n")
@@ -87,14 +87,14 @@ while True:
                 enemy_dmg = 1
 
             user_hp -= enemy_dmg
-            print(f"The {enemy_name} dealt {enemy_dmg} damage to you! Your HP:{user_hp}\n")
+            print(f"\tThe {enemy_name} dealt {enemy_dmg} damage to you! Your HP:{user_hp}\n")
             
     # Monster won - display a death message
             if user_hp <= 0:
-                    print(f"You have died in a embarassing way")
+                    print(f"You have died in a embarassing way\n")
 
     # Ask for if they want to star a new life
-        ans = input("The game stops here, wanna play again? (yes/no) ").lower().strip()
+        ans = input("\nThe game stops here, wanna play again? (yes/no) \n").lower().strip()
         if ans == "no":
             print("Thank you for playing the game!")
             break
